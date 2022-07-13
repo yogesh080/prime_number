3db3b212a27870289be4eacdff8ec65a902275a5
Yogesh <yogeshbietjhs@gmail.com>
2022-07-13 09:14:53 +0530

    Prime factor

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace prime_factorization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int num = 10;
+            int i,j;
+            int count = 0;
+            int flag = 0;
+
+            Console.WriteLine("");
+            for ( i = 2; i<num; i++)
+            {
+                if(num % i == 0)
+                {
+                    count = 0;
+                    for(j=1; j<=i; j++)
+                    {
+                        if (i % j == 0)
+                            count++;
+                    }
+                    if (count == 2)
+                    {
+                        flag =1;
+                        Console.WriteLine(i + " ");
+
+                    }
+                }
+                
+            }
+            if(flag == 0)
+                Console.WriteLine("no prime : " + num);
+        }
+    }
+}
